776c9e537090c860e4a0b0c2827b250a84410083
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-06-08 16:55:59 +0200

    t2/create gallery with lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryElement = document.querySelector("ul.gallery");
+
+const galleryItemsElements = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+        <li>
+            <a class="gallery__item" href="${original}">
+               <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `
+  )
+  .join("");
+
+galleryElement.innerHTML = galleryItemsElements;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
